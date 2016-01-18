5b1736da6140050829ed29c2052313085ffe179b
shihewhu <shiwhu@gmail.com>
2016-01-18 20:43:12 +0000

    fix some bugs because of change name

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'i7m@_az!#2aaq+*+(+^55k3_j4#-z0yywx0w2on9r1glxe&(14'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mysite',
+        'NAME': 'hosighost',
         'USER': 'root',
         'PASSWORD': '56099298xiao',
         'HOST': 'localhost',
